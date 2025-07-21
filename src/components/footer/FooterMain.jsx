58ffb70927d31c2109d01695213ba3df753b43b1
Gym Bigogwe <gymbigogwe@Bigogwes-iMac.local>
2025-07-21 17:29:30 +0200

    ..

--- src/components/footer/FooterMain.jsx
+++ src/components/footer/FooterMain.jsx
@@ -23,7 +23,7 @@ const FooterMain = () => {
     <div className="px-4">
       <div className="w-full h-[1px] bg-lightGrey mt-24"></div>
       <div className="md:flex justify-between mt-4 max-w-[1200px] mx-auto sm:hidden">
-        <p className="text-3xl text-lightGrey ">Abdelrahman Qassem</p>
+        <p className="text-3xl text-lightGrey ">Lievin Gakiza</p>
         <ul className="flex gap-4 text-lightGrey text-xl">
           {footerLinks.map((item, index) => {
             return (
