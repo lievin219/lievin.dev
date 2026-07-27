3da702e9f7db43930e35fe1be4f4393e706d6053
lievin219 <gakizalievin219@gmail.com>
2026-07-27 18:22:48 +0200

    added some final touches to be used in the development

--- postcss.config.js
+++ postcss.config.js
@@ -4,3 +4,10 @@ export default {
     autoprefixer: {},
   },
 }
+ function enumerateExperience(date,month){
+ let date=document.getElementById('date').ariaValueMax
+ let currentPosition=document.getElementById('currentposotion')
+ for(let i=0;i<10;i++){
+   let date=new Date()+i
+ }
+ }
